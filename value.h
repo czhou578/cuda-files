a3e6bfa39bf07e9f7b0ace9ceb55ce0724da4ae4
czhou578 <colizu2020@gmail.com>
2024-07-06 08:22:33 -0700

    commit

---
+++ value.h
@@ -0,0 +1,27 @@
+#include <cuda_runtime.h>
+#include <functional>
+#include <set>
+
+#ifndef Value
+#define Value
+
+class Value {
+public:
+  float data;
+  float grad;
+  std::set<Value *> prev;
+  std::string op;
+  std::function<void()> backward;
+
+  __host__ __device__ Value(float data,
+                            const std::vector<Value *> &children = {}, const)
+
+      __device__ void backward() {
+    backward();
+  }
+}
+
+__global__ void
+createValue(Value *v, float data) {
+  *v = Value(data);
+}
